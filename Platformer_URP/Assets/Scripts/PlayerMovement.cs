06697ecab40e5eb7339f72f876808784068652fe
Ayan <ayan1008@gmail.com>
2020-07-02 02:42:40 +0530

    Swipe input

--- Platformer_URP/Assets/Scripts/PlayerMovement.cs
+++ Platformer_URP/Assets/Scripts/PlayerMovement.cs
@@ -4,38 +4,43 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-	public CharacterController2D controller;
+    public CharacterController2D controller;
 
-	public float runSpeed = 40f;
-	public float dir = 1f;
-	float horizontalMove = 0f;
-	bool jump = false;
-	bool crouch = false;
+    public float runSpeed = 40f;
+    public float dir = 1f;
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
 
-	// Update is called once per frame
-	void Update()
-	{
-		if (Input.GetAxisRaw("Horizontal") != 0)
-			dir = Input.GetAxisRaw("Horizontal");
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0)
+            dir = Input.GetAxisRaw("Horizontal");
 
-		horizontalMove = dir * runSpeed;
+        horizontalMove = dir * runSpeed;
 
-		if (Input.GetButtonDown("Jump"))
-		{
-			jump = true;
-		}
-	}
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
 
-	void FixedUpdate()
-	{
-		// Move our character
-		controller.Move( horizontalMove * Time.fixedDeltaTime, jump);
-		jump = false;
-	}
+    void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
+    }
 
-	public void ChangeDirection()
-	{
-		float newDir = dir > 0 ? -1f : 1f;
-		dir = newDir;
-	}
+    public void ChangeDirection()
+    {
+        float newDir = dir > 0 ? -1f : 1f;
+        dir = newDir;
+    }
+
+    public void FaceTowardsDirection(float newDir)
+    {
+        dir = newDir;
+    }
 }
